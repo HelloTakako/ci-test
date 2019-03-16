d9172b9da671f6007d7abb2ba46e9584c729ed7a
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-03-16 16:19:55 -0700

    test

--- src/HannyaRoller.test.js
+++ src/HannyaRoller.test.js
@@ -6,24 +6,24 @@ describe('HannyaRoller', () => {
     expect(1 + 2).toBe(3);
   });
 
-  // /** @type HannyaRoller */
-  // let roller;
+  /** @type HannyaRoller */
+  let roller;
 
-  // /** @type HTMLDivElement */
-  // let el;
+  /** @type HTMLDivElement */
+  let el;
 
-  // beforeEach(() => {
-  //   el = window.document.createElement('div');
-  //   Object.defineProperty(el, 'clientWidth', { value: 800 });
-  //   Object.defineProperty(el, 'clientHeight', { value: 600 });
-  //   roller = new HannyaRoller({ el, text });
+  beforeEach(() => {
+    el = window.document.createElement('div');
+    Object.defineProperty(el, 'clientWidth', { value: 800 });
+    Object.defineProperty(el, 'clientHeight', { value: 600 });
+    roller = new HannyaRoller({ el, text });
 
-  //   roller.start();
-  // });
+    roller.start();
+  });
 
-  // it('sets up under the specified element', () => {
-  //   /** @type Element */
-  //   const elSpace = el.firstElementChild;
-  //   expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
-  // });
+  it('sets up under the specified element', () => {
+    /** @type Element */
+    const elSpace = el.firstElementChild;
+    expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
+  });
 });
