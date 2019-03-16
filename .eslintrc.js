0493b5a50d6a6343513e4cee5cbf61d984ea73cf
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-03-16 15:31:03 -0700

    'test'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    jest: true
   },
   extends: 'eslint-config-airbnb-base',
   rules: {
-    'arrow-parens': ['error', 'always']
+    'arrow-parens': ['error', 'always'],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true
+      }
+    ],
+    'class-methods-use-this': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'space-before-function-paren': ['error', 'always'],
+    'no-unused-vars': ['off']
   }
 };
